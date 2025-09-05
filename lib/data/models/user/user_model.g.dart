12872edd57200bce95cb2d490b8338cedf9995cc
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-09-05 20:39:02 +0300

    Fix UserModel object to be able to use freezed model generation properly

--- lib/data/models/user/user_model.g.dart
+++ lib/data/models/user/user_model.g.dart
@@ -6,15 +6,14 @@ part of 'user_model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$UserModelImpl _$$UserModelImplFromJson(Map<String, dynamic> json) =>
-    _$UserModelImpl(
-      id: json['id'] as String,
-      email: json['email'] as String,
-      displayName: json['displayName'] as String?,
-      photoUrl: json['photoUrl'] as String?,
-    );
+_UserModel _$UserModelFromJson(Map<String, dynamic> json) => _UserModel(
+  id: json['id'] as String,
+  email: json['email'] as String,
+  displayName: json['displayName'] as String?,
+  photoUrl: json['photoUrl'] as String?,
+);
 
-Map<String, dynamic> _$$UserModelImplToJson(_$UserModelImpl instance) =>
+Map<String, dynamic> _$UserModelToJson(_UserModel instance) =>
     <String, dynamic>{
       'id': instance.id,
       'email': instance.email,
