97ae431e5232942860e3aa5741ccbfedc94d6922
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-09-05 20:36:56 +0300

    Create a cache_constants file to keep hive related constants like box names, type ids, in a centeralized place

---
+++ lib/core/cache/cache_constants.dart
@@ -0,0 +1,12 @@
+/// Contains the names of all Hive boxes used in the application.
+abstract final class CacheBoxNames {
+  /// The box for storing notes.
+  static const String notes = 'notesBox';
+  static const String queue = 'queueBox';
+}
+
+/// Contains the type id's of all Hive classes used in application.
+abstract final class HiveTypeIds {
+  static const int note = 1;
+  static const int queue = 2;
+}
