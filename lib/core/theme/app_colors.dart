e89ee3b97e568e107a1f6094e402c3f0b9158b47
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-09-07 19:15:08 +0300

    Create a class to keep colors used in app in a single place.

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFF2F2F7);
+  static const Color cardBackground = Colors.white;
+  static const Color primaryText = Color(0xFF000000);
+  static const Color secondaryText = Color(0xFF6B6B6B);
+  static const Color accent = Color(0xFFFF9500);
+  static const Color border = Color(0xFFE5E5EA);
+  static const Color queueIndicator = Color(0xFFFF9500);
+}
